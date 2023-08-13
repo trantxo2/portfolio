dcc1305789f029b2844072ee8640fcbcfe1c96c4
trantxo2 <trantxo2@gmail.com>
2023-08-13 20:26:44 +0200

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,6 +13,19 @@ function Home() {
             lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
             ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
           </p>
+          <div className="socialmedia-container">
+            <div className="aqua-icon">
+              <a href="https://github.com/trantxo2">
+                <img src="/github.svg" alt="" />
+              </a>
+            </div>
+            <div className="aqua-icon">
+              <img src="/linkedin.svg" alt="" />
+            </div>
+            <div className="aqua-icon">
+              <img src="/instagram.svg" alt="" />
+            </div>
+          </div>
         </div>
         <div className="image-container">
           <img src="/avatar.png" alt=""></img>
