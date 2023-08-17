07a4efed7a8ede4c439de86065184d441283649f
trantxo2 <trantxo2@gmail.com>
2023-08-17 22:18:37 +0200

    estilacion home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,37 +11,33 @@ function Home() {
           <h1>Iñaki Trantxo</h1>
           <div className="info-details-container">
             <h2>Y soy </h2>
-            <h2 className="info-details-animated-text">
-              Desarrollador Frontend
-            </h2>
+            <h2 className="animated-text">Desarrollador Frontend</h2>
           </div>
           <p>
             lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
             ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
           </p>
           <div className="socialmedia-container">
-            <div id="media-icons">
+            <div id="media-icon">
               <a href="https://github.com/trantxo2">
-                <i className="bx bxl-github"></i>
+                <img src="/github.svg" alt="" />
               </a>
             </div>
             <div id="media-icons">
               <a href="https://www.linkedin.com/in/inaki09">
-                <i className="bx bxl-linkedin"></i>
+                <img src="/linkedin.svg" alt="" />
               </a>
             </div>
             <div id="media-icons">
-              <a href="https://github.com/trantxo2">
-                <i className="bx bxl-instagram"></i>
-              </a>
+              <img src="/instagram.svg" alt="" />
             </div>
           </div>
           <div className="downloadButton-container">
             <DownloadButton />
           </div>
         </div>
-        <div className="image-container">
-          <img className="profile-image" src="/profile.png" alt=""></img>
+        <div className="home-image-container">
+          <img className="home-image" src="/profile.png" alt=""></img>
         </div>
       </div>
     </div>
