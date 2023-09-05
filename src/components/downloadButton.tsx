d8322fc06ce4b336e14f80d2368b485b4a8cbc0f
trantxo2 <trantxo2@gmail.com>
2023-09-05 13:30:27 +0200

    actualizado link cv

--- src/components/downloadButton.tsx
+++ src/components/downloadButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DownloadButton = () => {
   const fileUrl =
-    'https://drive.google.com/file/d/1wDOXs7OStSPcmp2aaC3Z4jXNbPIk-OLz/view?usp=drive_link';
+    'https://drive.google.com/file/d/16ohslj54XYhfqtuwQXBi_20ISmOpOTkA/view?usp=sharing';
   const handleDownload = () => {
     window.open(fileUrl, '_blank');
   };
